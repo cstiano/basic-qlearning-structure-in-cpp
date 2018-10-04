7c9348c0874e6d0c12f4084d8207083d8ff7b1a0
cso <cso@cin.ufpe.br>
2018-10-04 17:32:25 -0300

    almost done with the structure

--- src/Qtablelearning.cpp
+++ src/Qtablelearning.cpp
@@ -1,5 +1,78 @@
 #include "Qtablelearning.h"
 
-Qtablelearning::Qtablelearning(){
-	
+Qtablelearning::Qtablelearning(const unsigned int states, const unsigned int actions){
+	this->states = states;
+	this->actions = actions;
+
+	this->Qtable = new double*[states];
+	for(int i = 0; i<actions; ++i){
+		this->Qtable[i] = new double[actions];
+	}
+
+	for (int i = 0; i < states; ++i)
+	{
+		for (int j = 0; j < actions; ++j)
+		{
+			this->Qtable[i][j] = 0;
+		}
+	}
+}
+
+std::pair<double,int> Qtablelearning::getMaxAction(const unsigned int curState){
+	double max = 0;
+	int index = 0;
+	for (int i = 0; i < this->actions; ++i)
+	{
+		if(this->Qtable[curState][i]>max){
+			max = this->Qtable[curState][i]; 
+			index = i;
+		}	
+	}
+	return std::make_pair(max, index);
+}
+
+double Qtablelearning::reward(){
+	//TODO
+}
+
+void Qtablelearning::updateQtable(){
+	//TODO
+}
+
+void Qtablelearning::saveTable(std::string tableName){
+	std::ofstream f(tableName);
+	for (int i = 0; i < this->states; i++){
+		for (int j = 0; j < this->actions; j++){
+	    	f << this->Qtable[i][j]<<" ";
+	  	}
+	}
+	f.close();
+}
+
+void Qtablelearning::saveTable(std::string tableName, std::string pathToSave){
+	std::ofstream f(pathToSave+"/"+tableName);
+	for (int i = 0; i < this->states; i++){
+		for (int j = 0; j < this->actions; j++){
+	    	f << this->Qtable[i][j]<<" ";
+	  	}
+	}
+	f.close();
+}
+
+void Qtablelearning::getTableFromFile(std::string tableName){
+	std::ifstream f(tableName);
+	for (int i = 0; i < this->states; i++){
+		for (int j = 0; j < this->actions; j++)
+		  f >> this->Qtable[i][j];
+	}
+	f.close();
+}
+
+void Qtablelearning::getTableFromFile(std::string tableName, std::string pathToGet){
+	std::ifstream f(pathToGet+"/"+tableName);
+	for (int i = 0; i < this->states; i++){
+		for (int j = 0; j < this->actions; j++)
+		  f >> this->Qtable[i][j];
+	}
+	f.close();
 }
--- src/Qtablelearning.h
+++ src/Qtablelearning.h
@@ -3,18 +3,47 @@
 
 #include <string>
 #include <iostream>
+#include <fstream>
 
 class Qtablelearning
 {
 private:
+	//States and actions to the fomulate Qtable
+	unsigned int states;
+	unsigned int actions;
+
+	//Qtable - matrix of double [states][actions]
+	double **Qtable;
+
+	//Const of Bellman equactions
+	double learningRate = 0.8;
+	double Gamma = 0.95;
 
 public:
-	Qtablelearning();
+	Qtablelearning(const unsigned int states, const unsigned int actions);
+	// Qtablelearning(const unsigned int states, const unsigned int actions, double lr, double g);
 	~Qtablelearning();
 	
-	//Save the q-learning table in a .txt
+	//Get max action in table with the state determined
+	std::pair<double,int> getMaxAction(const unsigned int curState);
+
+	//Get the reward of the environment
+	double reward();
+
+	//Function to update the Qtable (Q-learning table algorithm)
+	void updateQtable();
+
+	//Save the q-learning table in a file
 	void saveTable(std::string tableName);
 	void saveTable(std::string tableName, std::string pathToSave);
+
+	//Get table from file
+	void getTableFromFile(std::string tableName);
+	void getTableFromFile(std::string tableName, std::string pathToGet);
+
+	//Setting and configuring learning rate or gamma
+	void setLearningRate(double lr){ this->learningRate = lr; };
+	void setGamma(double g){ this->Gamma = g;};
 };
 
 #endif
